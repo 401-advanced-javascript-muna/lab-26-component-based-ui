d733adfedacbd1b630a6551153323d37b2ccd510
muna alshorman <munaalshorman123@gmail.com>
2020-02-24 15:05:55 +0200

    add polarity

--- my-app/src/modules/main.js
+++ my-app/src/modules/main.js
@@ -3,21 +3,36 @@ import React from 'react';
 class Main extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {counter:0}
+      this.state = {
+          counter:0,
+          polarity:''
   
     }
-  
+    }
     handleClickIncrese = e => {
       e.preventDefault();
       let counter = this.state.counter
       counter++;
       this.setState({ counter });
+      this.handlePolarity(counter)
     }
     handleClickDecrease = e => {
       e.preventDefault();
-      let counter = this.state.counter
+      let counter = this.state.counter;
       counter--;
       this.setState({ counter });
+      this.handlePolarity(counter)
+    }
+    handlePolarity=counter=>{
+        if (counter <0 ){
+         
+         let polarity = '-'
+          this.setState({polarity})};
+        if (counter >=0 ){
+          let polarity = '+'
+          this.setState({polarity})};
+
+
     }
   
     render() {
@@ -25,10 +40,14 @@ class Main extends React.Component {
         <section>
           <h3>{this.state.counter}</h3>
           <section>
-          <button onClick={this.handleClickIncrese}>click for increase </button>
+          <section>
+          <h3>polarity  {this.state.polarity}</h3>
+          {this.handlePolarity}
+          </section>
+          <button onClick={this.handleClickIncrese}> Increase </button>
           </section>
           <section>
-          <button onClick={this.handleClickDecrease}>click for decrease</button>
+          <button onClick={this.handleClickDecrease}> Decrease</button>
           </section>
         </section>
       )
